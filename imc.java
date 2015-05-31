9481c2728afeb5191660cb68e0813085fd4e4767
thiagosandano <thiagosandano@gmail.com>
2015-05-31 15:10:06 -0300

    Create imc.java

---
+++ imc.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+/**
+ *
+ * @author thiago
+ */
+public class imc {
+    public static float imc (float peso, float altura){
+        return peso / (altura * altura);
+    }
+    
+}
